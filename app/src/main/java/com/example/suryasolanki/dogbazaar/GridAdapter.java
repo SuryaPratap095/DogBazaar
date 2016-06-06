7103c651143fe0cb6e6644e8747bf991d71fc77f
Surya Solanki <surya.solanki@nihilent.com>
2016-06-06 17:51:41 +0530

    Grid Layout correction

--- app/src/main/java/com/example/suryasolanki/dogbazaar/GridAdapter.java
+++ app/src/main/java/com/example/suryasolanki/dogbazaar/GridAdapter.java
@@ -1,22 +1,36 @@
 package com.example.suryasolanki.dogbazaar;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by surya.solanki on 6/1/2016.
  */
 public class GridAdapter extends BaseAdapter{
-       // private Context bContext;
-       // private static final String breeds[]={ Dog,
+        private Context mContext;
+        private final String[] breeds;
+        private final int[] ImageId;
+
+    public GridAdapter(Context c, String[] breeds,int[] ImageId){
+        mContext=c;
+        this.breeds=breeds;
+        this.ImageId=ImageId;
+    }
+
+
         //}
     @Override
     public int getCount() {
-        return 0;
+
+        return breeds.length;
     }
 
     @Override
@@ -30,8 +44,30 @@ public class GridAdapter extends BaseAdapter{
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View grid;
+        LayoutInflater inflater=(LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        return null;
+        if(convertView==null){
+            grid=new View(mContext);
+            grid=inflater.inflate(R.layout.home_grid_single,null);
+            TextView textName=(TextView)grid.findViewById(R.id.grid_text);
+            LinearLayout linearGrid=(LinearLayout)grid.findViewById(R.id.gridLinear_image);
+            ImageView imgGrid=(ImageView)grid.findViewById(R.id.grid_image);
+            textName.setText(breeds[position]);
+            imgGrid.setImageResource(ImageId[position]);
+            linearGrid.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext,"You clicked At"+breeds[+position], Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else{
+            grid=(View)convertView;
+        }
+
+        return grid;
     }
 }
--- app/src/main/java/com/example/suryasolanki/dogbazaar/MainActivity.java
+++ app/src/main/java/com/example/suryasolanki/dogbazaar/MainActivity.java
@@ -4,16 +4,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridHome;
+    String[] breeds={
+            "Dogs",
+            "Cats",
+            "Birds",
+            "Accessories",
+            "Gallery",
+            "Contact Us"
+    };
+
+    int[] ImageId={
+            R.drawable.dog,
+            R.drawable.cat,
+            R.drawable.bird,
+            R.drawable.animalacc,
+            R.drawable.galleries,
+            R.drawable.contact
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GridView gridView=(GridView)findViewById(R.id.home_grid);
-        gridView.showContextMenu();
+
+        GridAdapter gridAdapter=new GridAdapter(MainActivity.this,breeds,ImageId);
+        gridHome=(GridView)findViewById(R.id.home_grid);
+        gridHome.setAdapter(gridAdapter);
+      /*  gridHome.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this,"You clicked At"+breeds[+position],Toast.LENGTH_SHORT).show();
+            }
+        });*/
 
     }
 
